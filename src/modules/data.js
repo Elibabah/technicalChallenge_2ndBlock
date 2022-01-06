3ab52398736d98ac147955a1b22091435ad0babe
Elibabah <delias16.hernandez@gmail.com>
2022-01-06 08:53:48 -0600

    activando firebase

--- src/modules/data.js
+++ src/modules/data.js
@@ -1,9 +1,13 @@
+import * as module from "./firebase.js"
+
+const db = firebase.firestore();
+//db.collection("metas").add()
+
+
 export const enviarFB = () => {
-
-
     let botonEnviarMeta = document.getElementById("enviarMeta")
 
-    botonEnviarMeta.addEventListener("click", () => {
+    botonEnviarMeta.addEventListener("click", async(e) => {
         let metaObject = {
             "tipo": document.getElementById("tipo").value,
             "titulo": document.getElementById("titulo").value,
