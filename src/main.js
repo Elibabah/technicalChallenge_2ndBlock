fef8d61cc4f619716223aaafe2acc38bdfdb433e
Elibabah <delias16.hernandez@gmail.com>
2022-01-06 00:14:15 -0600

    readme ready

---
+++ src/main.js
@@ -0,0 +1,4 @@
+import { connected } from "./modules/app.js";
+connected()
+
+alert("main.js connected")
---
+++ src/modules/app.js
@@ -0,0 +1,3 @@
+export let connected = () => {
+    console.log("app.js module connected")
+}
